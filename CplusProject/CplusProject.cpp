7fae9fc88eee30c022715e9521e0d21596d6a12d
Evan Colombus <c013709o@student.staffs.ac.uk>
2024-10-02 10:30:03 +0100

    initial commit

---
+++ CplusProject/CplusProject.cpp
@@ -0,0 +1,20 @@
+﻿// CplusProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int ranNum = rand() % 100;
+	const char *logoStaffs = R""""(
+
+		   _____ __        ________    
+		  / ___// /_____ _/ __/ __/____
+		  \__ \/ __/ __ `/ /_/ /_/ ___/
+		 ___/ / /_/ /_/ / __/ __(__  ) 
+		/____/\__/\__,_/_/ /_/ /____/  
+                               
+	)"""";
+	cout << logoStaffs;
+	cout << ranNum;
+}
